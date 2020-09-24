0860136576109177367cf9e4ab6bf4ce99850f04
mdfsar15 <57721986+mdafsar15@users.noreply.github.com>
2020-09-24 12:08:31 +0530

    [AFSAR]ADDED: ADF Icon

---
+++ ViewController/src/view/backing/AdfIcon.java
@@ -0,0 +1,25 @@
+package view.backing;
+
+import oracle.adf.view.rich.component.rich.RichDocument;
+import oracle.adf.view.rich.component.rich.RichForm;
+
+public class AdfIcon {
+    private RichForm f1;
+    private RichDocument d1;
+
+    public void setF1(RichForm f1) {
+        this.f1 = f1;
+    }
+
+    public RichForm getF1() {
+        return f1;
+    }
+
+    public void setD1(RichDocument d1) {
+        this.d1 = d1;
+    }
+
+    public RichDocument getD1() {
+        return d1;
+    }
+}
